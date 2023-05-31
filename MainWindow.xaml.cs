c4caad16f16b4e3c14f1fc3fe705744c0584a8ac
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-05-31 09:01:32 +0200

    Init commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dart
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static int NoOfPlayers = 0;
+        public static int RoundNo = 0;
+        public static string Filelocation = "./Files";
+        public static List<string> Players = new List<string>();
+         
+        public void RefreshPlayers()
+        {
+            CurrentPlayerList.Text = null;
+            foreach (var player in Players)
+            {
+                CurrentPlayerList.Text += $"{player}\n";
+            }
+        }
+        public void InitEnv()
+        {
+            if (! File.Exists($"{Filelocation}/players.txt"))
+            {
+            Directory.CreateDirectory(Filelocation);
+            File.Create($"{Filelocation}/players.txt");
+            }
+            Players.Clear();
+            foreach (string line in File.ReadLines($"{Filelocation}/players.txt"))
+            {
+                Players.Add(line);
+            }
+            NoOfPlayers = Players.Count;
+        }
+            public MainWindow()
+        {
+            InitializeComponent();
+            InitEnv();
+            RefreshPlayers();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var listwindows = new PlayerListPage();
+            Close();
+            listwindows.Show();
+        }
+    }
+}
---
+++ PlayerListPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dart
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class PlayerListPage : Window
+    {
+        public void RefreshPlayers()
+        {
+            PlayerList.Items.Clear();
+            foreach (var player in MainWindow.Players)
+            {
+                PlayerList.Items.Add(player);
+            }
+        }
+        public void RefreshPlayerstxt()
+        {
+            File.WriteAllText($"{MainWindow.Filelocation}/players.txt", "");
+            File.WriteAllLines($"{MainWindow.Filelocation}/players.txt", MainWindow.Players);
+            MainWindow.NoOfPlayers = MainWindow.Players.Count;
+        }
+        public PlayerListPage()
+        {
+            InitializeComponent();
+            RefreshPlayers();
+
+            
+        }
+
+        private void AddPlayerButton_Click(object sender, RoutedEventArgs e)
+        {
+            string UserAnswer = Microsoft.VisualBasic.Interaction.InputBox("Please write the name below: ", "Name", "");
+            if (UserAnswer != "")
+            {
+                MainWindow.Players.Add(UserAnswer);
+                RefreshPlayerstxt();
+                RefreshPlayers();
+            }
+          
+            
+
+
+        }
+
+        private void PlayerList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (PlayerList.SelectedItem != null)
+            {
+                string item = PlayerList.SelectedItem.ToString();
+                if (MessageBox.Show($"Delete {item} from player list?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    MainWindow.Players.Remove(item);
+                    RefreshPlayerstxt();
+                    RefreshPlayers();
+                       
+                }
+            }
+            
+
+      
+
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var Mainwin = new MainWindow();
+            Mainwin.Show();
+        }
+    }
+}
